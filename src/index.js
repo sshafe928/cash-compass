b39df3fdd0376ebfed3b8d3626d1f0993b58ca24
Daniel <141285622+DanielFreeman068@users.noreply.github.com>
2025-02-28 14:20:12 -0700

    implemented frontend files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
-import Dashboard from './pages/Dashboard';
-import Form from './pages/DSIEForm';
+import Dashboard from './pages/App';
+import Forms from './pages/Forms';
+import Budget from './pages/Budget';
+import History from './pages/History';
+import About from './pages/About';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const router = createBrowserRouter([
-  { path: '/', element: <Dashboard/>},
-  { path: '/forms', element: <Form/>},
-  // Add more routes here
+  { path: '/', element: <Dashboard /> },
+  { path: '/forms', element: <Forms /> },
+  { path: '/history', element: <History /> },
+  { path: '/budget', element: <Budget /> },
+  { path: '/about', element: <About />},
+  { path: '/login', element: <Login />},
+  { path: '/signup', element: <Signup />},
 ])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
