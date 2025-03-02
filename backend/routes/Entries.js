9c88103efb3336288b1e327f22ace0e5e0600c00
Sabrina Shafer <sshafe928@west-mec.org>
2025-03-02 10:36:11 -0700

    routing fixes

--- backend/routes/Entries.js
+++ backend/routes/Entries.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const Saving = require('../models/Savings_entry');
-const Debt = require('../models/Debt_entry');
+const Saving = require('../models/Savings');
+const Debt = require('../models/debtItems');
 const Expense = require('../models/Expense_entry');
 const Income = require('../models/Income_entry');
+const Goals = require('../models/savingsItems');
+const Budgets = require('../models/budgetItems');
 const isAuth = require('../middleware/isAuth');
 
 
